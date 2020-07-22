c8461aacdb73f358c78c9b2edd622423461be177
Peder Garberg <pgarberg@gmail.com>
2020-07-21 21:51:30 -0400

    Grider Assistance

--- index.js
+++ index.js
@@ -279,6 +279,13 @@ app.post("/mealplan", async (req, res) => {
   }
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static('client/build')));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen("4000", () => {
   console.log("App is up and running on 4000");
 });
