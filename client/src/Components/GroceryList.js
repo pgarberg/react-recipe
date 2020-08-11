c7192320cbcc318d3aa8a1e05e3828fa6bf53e28
Peder Garberg <pgarberg@gmail.com>
2020-08-10 22:43:07 -0400

    Added Regex to Grocery List

--- client/src/Components/GroceryList.js
+++ client/src/Components/GroceryList.js
@@ -14,16 +14,33 @@ export const GroceryList = () => {
       }
     });
   }
-  console.log("groceryList :>> ", groceryList);
+  let transformedGroceryList = [];
+
+  groceryList.map((item) => {
+    let p = item;
+    const regex = /[0-9]+\/*[a-z]*|\u00BE|\u00BD|\u00BC |\*|\/|:|-|\.|,.*$|\([^)]+\)|\b(tin|fresh|ripe)\b|\b(cup|tsp|tablespoon|water|tbsp|trimmed|tablespoons|medium|large|small|clove|cloves|pound|pounds|oz|ounces|lb|lbs|teaspoons|teaspoon|cups|chopped|diced|minced|tblspn)\b/gim;
+    let newP = p.replace(regex, "").trim();
+    console.log("newP", newP);
+    console.log("transformedGroceryList", transformedGroceryList);
+    console.log(
+      "transformedGroceryList.includes(newP)",
+      transformedGroceryList.includes(newP)
+    );
+
+    if (!transformedGroceryList.includes(newP) && newP !== "") {
+      transformedGroceryList.push(newP);
+    }
+  });
+
   return (
     <div className="container">
       <h3>GroceryList</h3>
       <hr className="mb-4" />
 
       <ul>
-        {groceryList.map((item) => (
-          <li>{item}</li>
-        ))}
+        {transformedGroceryList.map((item) => {
+          return <li>{item}</li>;
+        })}
       </ul>
     </div>
   );
