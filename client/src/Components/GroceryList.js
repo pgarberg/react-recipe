91a149ead8d98d5007b97f80e6aa1f1c90b8c606
Peder Garberg <pgarberg@gmail.com>
2020-08-23 14:52:15 -0400

    Adding Saved Meal Plans Routes and Components

--- client/src/Components/GroceryList.js
+++ client/src/Components/GroceryList.js
@@ -19,7 +19,7 @@ export const GroceryList = () => {
   groceryList.map((item) => {
     let p = item;
     const regex = /\s(g|kg|kgs|lbs|mL|L|grams|cps|cups|tblspns|tspn)\/|\s(g|kg|kgs|lbs|mL|L|grams|cps|cups|tblspns|tspn)\/|^(g|kg|kgs|lbs|mL|L|grams|cps|cups|tblspns|tspn)\s|\s(g|kg|kgs|lbs|mL|L|grams|cps|cups|tblspns|tspn)\s|[0-9]+\s+\w+\s+[0-9]+\s+|[0-9]+\/*[a-z]*|\u00BE|\u00BD|\u00BC |\[.+?\]\((.*)\)|\((.*)\((.*)\)(.*)\)|\*|\/|:|-|\.|,.*$|\([^)]+\)|\b(tin|fresh|ripe)\b|\b(cup|tsp|tablespoon|water|tbsp|trimmed|julienned|tablespoons|medium|large|small|clove|cloves|pound|pounds|oz|ounces|lb|lbs|teaspoons|fresh|freshly|cracked|fine|for|to|get|finely|of|chopped|shredded|teaspoon|cups|chopped|diced|minced|package|leaves|zest|big|handful|sliced|packages|few|many|pinch|tblspn)\b|or$|or\s$/gim;
-    const regex2 = /or\s$|or$/s;
+    const regex2 = /or\s$|or$\/s/;
     let newP = p.replace(regex, "").trim();
     if (newP.includes("lemongrass")) {
       console.log(newP);
